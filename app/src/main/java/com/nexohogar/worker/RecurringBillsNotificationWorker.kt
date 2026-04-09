a8b49ce3e9b306d145ea243241cfbb4c4e376e7a
exekussGB <exekuss@gmail.com>
2026-04-09 11:43:01 -0400

    Reparando Bugs

--- app/src/main/java/com/nexohogar/worker/RecurringBillsNotificationWorker.kt
+++ app/src/main/java/com/nexohogar/worker/RecurringBillsNotificationWorker.kt
@@ -123,7 +123,7 @@ class RecurringBillsNotificationWorker(
         )
 
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(R.drawable.ic_launcher_foreground_xxxhdpi)
             .setContentTitle(title)
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
